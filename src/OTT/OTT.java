ca696158b94bb51241d5d302adc4ce5a946de207
André Martins <accmartins3000@gmail.com>
2021-11-23 10:22:01 +0000

    Cenas

--- src/OTT/OTT.java
+++ src/OTT/OTT.java
@@ -1,5 +1,9 @@
 package OTT;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class OTT {
     public static void main(String[] args){
         System.out.println("Sou o OTT");
